06a71a503b3b1bd1165170cfbf1aa831be8629cb
Damjan Roskar <damjan.roskar.s@gmail.com>
2014-12-07 12:11:38 +0100

    Player and Game classes added

---
+++ BGCRanker/Game.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGCRanker
+{
+    class Game
+    {
+
+        public String Name { get; set; }
+        public Boolean HasRankingLadder { get; set; }
+        private List<Player> players;
+        private List<String> playerNames;
+
+        public Game()
+        {
+            players = new List<Player>();
+            playerNames = new List<String>();
+        }
+
+        public Game(String name)
+        {
+            this.Name = name;
+
+            players = new List<Player>();
+            playerNames = new List<String>();
+        }
+
+        public Game(String name, Boolean hasRankingLadder)
+        {
+            this.Name = name;
+            this.HasRankingLadder = hasRankingLadder;
+
+            players = new List<Player>();
+            playerNames = new List<String>();
+        }
+
+        public void addPlayer(Player player)
+        {
+            players.Add(player);
+
+            // add player to list of names
+            playerNames.Add(player.Name);
+        }
+
+        public void setPlayers(List<Player> players)
+        {
+
+            this.players = players;
+
+            // add players to list of names
+            playerNames.Clear();
+            foreach(Player player in players){
+                playerNames.Add(player.Name);
+            }
+        }
+
+        public List<Player> getPlayers()
+        {
+            return players;
+        }
+
+        public List<String> getPlayerNames()
+        {
+            return playerNames;
+        }
+    }
+}
---
+++ BGCRanker/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGCRanker
+{
+    class Player
+    {
+        public String Name { get; set; }
+        public String Game { get; set; }
+        public int Victories { get; set; }
+        public int Level { get; set; }
+        public String Rank { get; set; }
+
+        public Player()
+        {
+
+        }
+
+        public Player(String name)
+        {
+            this.Name = name;
+        }
+
+        public Player(String name, String game)
+        {
+            this.Name = name;
+            this.Game = game;
+        }
+    }
+}
