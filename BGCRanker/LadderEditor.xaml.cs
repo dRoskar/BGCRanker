9fd19734fc89f1ff14c8b9d4acf88c4c41a0a1f6
Damjan Roskar <damjan.roskar.s@gmail.com>
2014-12-24 13:36:54 +0100

    Ladder Editor refreshes grid if properties are changed or on command

--- BGCRanker/LadderEditor.xaml.cs
+++ BGCRanker/LadderEditor.xaml.cs
@@ -29,7 +29,6 @@ namespace BGCRanker
         private float formula;
         private bool isCustom;
         private bool hasData;
-        private List<float> requirements;
         private ObservableCollection<Level> ladder;
 
         public LadderEditor(String ladderPath)
@@ -41,14 +40,12 @@ namespace BGCRanker
 
         private void DataGrid_Loaded(object sender, RoutedEventArgs e)
         {
-            // bind data
+            // setup grid
             ranksGrid.ItemsSource = ladder;
+            ranksGrid.ColumnFromDisplayIndex(0).IsReadOnly = true;
 
             // get main properties
             getProperties();
-
-            // calculate level requirements
-            calculateRequirements();
 
             // display main properties
             showProperties();
@@ -93,8 +90,9 @@ namespace BGCRanker
         private void showProperties()
         {
             levelsTextBox.Text = levels.ToString();
-            formulaTextBox.Text = formula.ToString();
+            formulaTextBox.Text = formula.ToString(CultureInfo.InvariantCulture);
             isCustomCheckBox.IsChecked = isCustom;
+            formulaTextBox.IsEnabled = !isCustom;
         }
 
         private void generateGridData(){
@@ -141,20 +139,16 @@ namespace BGCRanker
 
         private int getRequirement(int level)
         {
-            return (int) Math.Round(requirements[level - 1], 0);
-        }
+            if (level == 1) { return 0; }
+            if (level == 2) { return 1; }
 
-        private void calculateRequirements()
-        {
-            requirements = new List<float>();
-            requirements.Add(0.0f);
-            requirements.Add(1.0f);
-            float requirement = requirements[1];
-            for (int i = 0; i < levels - 2; i++)
+            float requirement = 1.0f;
+            for (int i = 0; i < level - 2; i++)
             {
                 requirement += requirement * formula;
-                requirements.Add(requirement);
             }
+
+            return (int)Math.Round(requirement, 0);
         }
 
         private void writeIsCustom(Boolean value)
@@ -186,9 +180,124 @@ namespace BGCRanker
             this.Close();
         }
 
-        private void cancleBtn_Click(object sender, RoutedEventArgs e)
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void levelsTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.Equals(Key.Enter))
+            {
+                refreshGrid();
+            }
+        }
+
+        private void formulaTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.Equals(Key.Enter))
+            {
+                refreshGrid();
+            }
+        }
+
+        private void refreshBtn_Click(object sender, RoutedEventArgs e)
+        {
+            refreshGrid();
+        }
+
+        private void refreshGrid()
+        {
+            // validate fields
+            int tempLevels;
+            float tempFormula;
+            Boolean valid = true;
+            String invalidField = null;
+            Boolean multipleInvalidFields = false;
+            if (!int.TryParse(levelsTextBox.Text, out tempLevels))
+            {
+                valid = false;
+                invalidField = "Levels text box";
+            }
+
+            if (!float.TryParse(formulaTextBox.Text, System.Globalization.NumberStyles.Any, NumberFormatInfo.InvariantInfo, out tempFormula))
+            {
+                valid = false;
+
+                if (invalidField == null)
+                {
+                    invalidField = "formula text box";
+                }
+                else{
+                    invalidField += "and the formula text box";
+                    multipleInvalidFields = true;
+                }
+            }
+
+            if (valid)
+            {
+                // edit grid data (levels)
+                if (tempLevels != levels)
+                {
+                    if (tempLevels < levels)
+                    {
+                        // removing levels
+                        levels = tempLevels;
+                        for (int i = ladder.Count - 1; i > levels - 1; i--)
+                        {
+                            ladder.Remove(ladder[i]);
+                        }
+                    }
+                    else
+                    {
+                        // adding levels
+                        levels = tempLevels;
+                        for (int i = ladder.Count; i < levels; i++)
+                        {
+                            ladder.Add(new Level(i + 1, getRequirement(i + 1), ""));
+                        }
+                    }
+                }
+
+                if (!isCustom)
+                {
+                    // edit grid data (requirements)
+                    if (tempFormula != formula)
+                    {
+                        formula = tempFormula;
+                        foreach (Level lvl in ladder)
+                        {
+                            lvl.Requirement = getRequirement(lvl.Number);
+                        }
+                    }
+                }
+
+                ranksGrid.Items.Refresh();
+            }
+            else
+            {
+                if (multipleInvalidFields)
+                {
+                    MessageBox.Show("The values you provided in the " + invalidField + " are invalid");
+                }
+                else
+                {
+                    MessageBox.Show("The value you provided in the " + invalidField + " is invalid");
+                }
+
+            }
+        }
+
+        private void isCustomCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            isCustom = true;
+            formulaTextBox.IsEnabled = false;
+        }
+
+        private void isCustomCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            isCustom = false;
+            formulaTextBox.IsEnabled = true;
         }
     }
 
